2a237963bb652b2da9531850a53af2e25d075a08
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2024-10-20 13:45:04 +0800

    save data at the function level

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,11 @@
 pytest_plugins = "pytester"
+
+import os
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="function")
+def original_datadir(request) -> Path:
+    return Path(os.path.splitext(request.module.__file__)[0]) / request.function.__name__
